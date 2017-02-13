3c36eecf3e15a5eaf34b24834af267cf4355c2a7
michaelhmt <hindleymht@gmail.com>
2017-02-13 10:48:21 +0000

    Made it so the player can only jump when they do not have a velocity in the Y axsis and also fixed player control script so jump power can be set in editior.

--- Diffuze_2D/Assets/Scripts/PlayerControl.cs
+++ Diffuze_2D/Assets/Scripts/PlayerControl.cs
@@ -8,33 +8,40 @@ public class PlayerControl : MonoBehaviour {
 	public float moveforce = 5;
 	public float jumpPower = 550;
 	private bool jumping;  
+	bool IsGrounded = true;
 
 	// Use this for initialization
 	void Start () {
-		bBody = this.GetComponent<Rigidbody2D> ();
-		jumping = false;
+		bBody = this.GetComponent<Rigidbody2D> ();  //finds the rigidbody of connected object
+		jumping = false;   //player is not jumping at start 
 		
 	}
-	
-	// Update is called once per frame
 	void FixedUpdate () {
 	
-		Vector2 movevec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"),CrossPlatformInputManager.GetAxis("Vertical")) * moveforce; 
-		bBody.AddForce (movevec);
-		if (CrossPlatformInputManager.GetButton ("JUMP")) {
+		Vector2 movevec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"),CrossPlatformInputManager.GetAxis("Vertical")) * moveforce;  //adds force based on input from the UI joystick
+		bBody.AddForce (movevec);     //adds that force to rigidbody2D
+		if (CrossPlatformInputManager.GetButton ("JUMP")) { //if jump button is active fot this update do...
 			jump ();
 
 		}
+		if (bBody.velocity.y < -0.1) {
+			IsGrounded = false; 
+
+		} else {
+
+			IsGrounded = true; 
+		}
+
 
 	}
 
 
 	void jump(){
 
-
-		bBody.AddForce(new Vector2(0,450));
-		Debug.Log ("Jump");
-
+		if (IsGrounded == true) {
+			gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpPower);
+			Debug.Log ("Jump");
+		}
 	}
 
 
