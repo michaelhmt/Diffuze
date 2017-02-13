f12623cd91fa97dd48a8821650f13417936df160
michaelhmt <hindleymht@gmail.com>
2017-02-13 11:15:19 +0000

    fixed a bug where player could boost jump themselves by holding the jump button when they land from a jump.

--- Diffuze_2D/Assets/Scripts/PlayerControl.cs
+++ Diffuze_2D/Assets/Scripts/PlayerControl.cs
@@ -9,6 +9,7 @@ public class PlayerControl : MonoBehaviour {
 	public float jumpPower = 550;
 	private bool jumping;  
 	bool IsGrounded = true;
+	public float jumpDelay = 3.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,17 +21,22 @@ public class PlayerControl : MonoBehaviour {
 	
 		Vector2 movevec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"),CrossPlatformInputManager.GetAxis("Vertical")) * moveforce;  //adds force based on input from the UI joystick
 		bBody.AddForce (movevec);     //adds that force to rigidbody2D
-		if (CrossPlatformInputManager.GetButton ("JUMP")) { //if jump button is active fot this update do...
+		if (CrossPlatformInputManager.GetButtonUp ("JUMP")) { //if jump button is active fot this update do...
 			jump ();
 
 		}
-		if (bBody.velocity.y < -0.1) {
+		if (bBody.velocity.y < -1.0) {
 			IsGrounded = false; 
 
 		} else {
-
+			if(jumpDelay >= 0 ){
+				jumpDelay -= Time.deltaTime;
+				return;
+			}else{
 			IsGrounded = true; 
+			}
 		}
+
 
 
 	}
@@ -39,10 +45,12 @@ public class PlayerControl : MonoBehaviour {
 	void jump(){
 
 		if (IsGrounded == true) {
-			gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpPower);
+			gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, jumpPower);
 			Debug.Log ("Jump");
 		}
+
 	}
+
 
 
 }
