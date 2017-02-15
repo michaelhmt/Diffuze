ca0e99150722f911b14012a7442f77ca97f043a9
michaelhmt <hindleymht@gmail.com>
2017-02-15 19:42:48 +0000

    Fixed bug with player controllor, I hope

--- Diffuze_2D/Assets/Scripts/PlayerControl.cs
+++ Diffuze_2D/Assets/Scripts/PlayerControl.cs
@@ -11,6 +11,7 @@ public class PlayerControl : MonoBehaviour {
 	public float jumpPower = 550;
 	public bool jumping;  
 	bool IsGrounded = true;
+	public float jumpDelay = 3.0f;
 	private float InVertedJumpLimt = -0.1f; 
 	private float jumpcache = 0.1f;
 
@@ -20,8 +21,8 @@ public class PlayerControl : MonoBehaviour {
 		bBody = this.GetComponent<Rigidbody2D> ();  //finds the rigidbody of connected object
 		jumping = false;   //player is not jumping at start 
 
-		jumpcache = minimum_jump_limt * 2; 
-		InVertedJumpLimt = minimum_jump_limt - jumpcache;  // finds the negative inversion of the jump limt 
+		jumpcache = minimum_jump_limt * 2;
+		InVertedJumpLimt = minimum_jump_limt - jumpcache;
 
 		
 	}
@@ -29,18 +30,26 @@ public class PlayerControl : MonoBehaviour {
 	
 		Vector2 movevec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"),CrossPlatformInputManager.GetAxis("Vertical")) * moveforce;  //adds force based on input from the UI joystick
 		bBody.AddForce (movevec);     //adds that force to rigidbody2D
-
 		if (CrossPlatformInputManager.GetButtonUp ("JUMP")) { //if jump button is active fot this update do...
 			jump ();
 		}
-
-	
-		if (bBody.velocity.y < InVertedJumpLimt) {   //check to see if player is falling faster than jump limt 
+		if (bBody.velocity.y < InVertedJumpLimt) {   //check to see if player is falling
 				IsGrounded = false;
 				jumping = true;
+
+
+			} else {                      //jump delay unimplemented right now
+				if (jumpDelay >= 0) {
+					jumpDelay -= Time.deltaTime;
+					return;
+				} else {
+					IsGrounded = true; 
+					jumping = false;
+				}
 			}
-		if (bBody.velocity.y > minimum_jump_limt) {   //check to see if player is going up faster than jump limt 
+		if (bBody.velocity.y > minimum_jump_limt) {   //check to see if player is going up
 			jumping = true; 
+		
 		}
 	}
 
