64d8f27039407293160e6b3c687c2de9fbd7a4af
Rui-Fan <ruifan@hotmail.co.uk>
2017-02-17 16:34:58 +0000

    Reset Button
    Hope this works

---
+++ Diffuze_2D/Assets/Scripts/ResetScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScript : MonoBehaviour
+{
+
+    public void Reset()
+    {
+        int scene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+
+
+
+}
