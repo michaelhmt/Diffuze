e546f6a7d5f46186113338865fbd311fb1beabfa
michaelhmt <hindleymht@gmail.com>
2017-02-18 20:47:27 +0000

    fixed level ending bug

--- Diffuze_2D/Assets/Scripts/Game_end.cs
+++ Diffuze_2D/Assets/Scripts/Game_end.cs
@@ -6,8 +6,13 @@ public class Game_end : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other)
     {
-            Application.LoadLevel(0);
-        Debug.Log("I'm so turned on right now you don't even know!");
-        }
+		if (other.tag == "Player") {
+			Application.LoadLevel (0);
+			Debug.Log ("I'm so turned on right now you don't even know!");
+
+		}
+
+       }
+
     }
 
