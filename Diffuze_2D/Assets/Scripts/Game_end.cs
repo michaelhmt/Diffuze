f913d110aeec65ce61cc2c1e45bc02bb5a329d17
Andreas Karpasitis <andreas_karpasitis@hotmail.com>
2017-02-18 19:20:19 +0000

    added some cool stuff bro!
    Added:
    animations for the pick-ups
    animated portal for level completion

---
+++ Diffuze_2D/Assets/Scripts/Game_end.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_end : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+            Application.LoadLevel(0);
+        Debug.Log("I'm so turned on right now you don't even know!");
+        }
+    }
+
