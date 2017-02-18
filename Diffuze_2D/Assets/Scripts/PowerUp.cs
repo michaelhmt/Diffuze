c52561cbf1914b867f3b8f5afc00ccca3c5447bc
Rui-Fan <ruifan@hotmail.co.uk>
2017-02-18 11:18:07 +0000

    PowerUp
    Thanks Michael :)

---
+++ Diffuze_2D/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+    public GameObject Player;
+    public PlayerScript Pup;
+    public GameObject TimeObject;
+    public TimerScript TimerScript;
+
+
+    void OnTriggerEnter2D(Collider2D myTrigger)
+    {
+
+        Debug.Log("it hit the thing");
+
+        if (myTrigger.tag == "Player")
+        {
+            Pup.Speedboost();
+            TimerScript.TStart();
+            Destroy(gameObject);
+        }
+    }
+
+}
